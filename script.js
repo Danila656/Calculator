58fa5fb4e3b4db3631616d5aa0aa8e696d80603c
Danila Zankevich <zankevich.danya535@gmail.com>
2020-02-16 16:50:20 +0300

    bug fixes 2.0

--- script.js
+++ script.js
@@ -1,156 +1,72 @@
 let string = ' ';
 let example = '';
+let sign;
 
-
-function shortenString(){
-    if (size>13){
-        string=string.slice(1);
+let shortenString = () => {
+    if (example.length > 13) {
+        string = string.slice(1);
     }
-}
+};
 
-function changeOp() {
-    let sign=example.slice(example.length-1);
-    switch (sign){
+let checkExample = () => {
+    if (example.length === 1) {
+        if (example === '/' || example==='*' || example==='.' || example==='+') {
+            example='';
+            string=example;
+        }
+    }
+
+};
+
+let changeOp = () => {
+    sign = example.slice(example.length - 1);
+    switch (sign) {
         case '/':
-            string=string.slice(0,string.length-1);
-            example=example.slice(0,example.length-1);
-        break;
+            string = string.slice(0, string.length - 1);
+            example = example.slice(0, example.length - 1);
+            break;
         case '*':
-            string=string.slice(0,string.length-1);
-            example=example.slice(0,example.length-1);
+            string = string.slice(0, string.length - 1);
+            example = example.slice(0, example.length - 1);
             break;
         case '-':
-            string=string.slice(0,string.length-1);
-            example=example.slice(0,example.length-1);
+            string = string.slice(0, string.length - 1);
+            example = example.slice(0, example.length - 1);
             break;
         case '+':
-            string=string.slice(0,string.length-1);
-            example=example.slice(0,example.length-1);
+            string = string.slice(0, string.length - 1);
+            example = example.slice(0, example.length - 1);
             break;
         case '.':
-            string=string.slice(0,string.length-1);
-            example=example.slice(0,example.length-1);
+            string = string.slice(0, string.length - 1);
+            example = example.slice(0, example.length - 1);
             break;
-        default:
-            break;
-        }
-}
+    }
+};
 
-
-function one() {
-    string = string + '1';
-    example = example + '1';
-    document.getElementById('answer').innerHTML = string;
+let number = (num) => {
+    string = string + num;
+    example = example + num;
     shortenString();
-}
-
-function two() {
-    string = string + '2';
-    example = example + '2';
     document.getElementById('answer').innerHTML = string;
-    shortenString();
-}
 
-function three() {
-    string = string + '3';
-    example = example + '3';
-    document.getElementById('answer').innerHTML = string;
-    shortenString();
-}
+};
 
-function four() {
-    string = string + '4';
-    example = example + '4';
-    document.getElementById('answer').innerHTML = string;
-    shortenString();
-}
-
-function five() {
-    string = string + '5';
-    example = example + '5';
-    document.getElementById('answer').innerHTML = string;
-    shortenString();
-}
-
-function six() {
-    string = string + '6';
-    example = example + '6';
-    document.getElementById('answer').innerHTML = string;
-    shortenString();
-}
-
-function seven() {
-    string = string + '7';
-    example = example + '7';
-    document.getElementById('answer').innerHTML = string;
-    shortenString();
-
-}
-
-function eight() {
-    string = string + '8';
-    example = example + '8';
-    document.getElementById('answer').innerHTML = string;
-    shortenString();
-}
-
-function nine() {
-    string = string + '9';
-    example = example + '9';
-    document.getElementById('answer').innerHTML = string;
-    shortenString();
-}
-
-function zero() {
-    string = string + '0';
-    example = example + '0';
-    document.getElementById('answer').innerHTML = string;
-    shortenString();
-}
-
-function multiply() {
+let op = (op) => {
     changeOp();
-    string = string + '×';
-    example = example + '*';
+    string = string + op;
+    example = example + op;
+    checkExample();
     document.getElementById('answer').innerHTML = string;
     shortenString();
-}
+};
 
-function divide() {
-    changeOp();
-    string = string + '÷';
-    example = example + '/';
-    document.getElementById('answer').innerHTML = string;
-    shortenString();
-}
-
-function minus() {
-    changeOp();
-    string = string + '-';
-    example = example + '-';
-    document.getElementById('answer').innerHTML = string;
-    shortenString();
-}
-
-function plus() {
-    changeOp();
-    string = string + '+';
-    example = example + '+';
-    document.getElementById('answer').innerHTML = string;
-    shortenString();
-}
 
 function same() {
     example = eval(example);
     string = example;
     document.getElementById('answer').innerHTML = example;
-}
-
-function point() {
-    changeOp();
-    string = string + '.';
-    example = example + '.';
-    document.getElementById('answer').innerHTML = string;
+    example += '';
 }
 
 function remove() {
